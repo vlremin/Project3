09421391a269d3ef66303df323ab4c243d77ad24
VALERIE ONG XUE MIN 2004141D <2004141D@student.tp.edu.sg>
2022-02-09 03:02:41 +0800

    Add project files.

---
+++ Server/Configurations/TripSeedConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Project3.Shared.Domain;
+
+namespace Project3.Server.Configurations
+{
+    public class TripSeedConfiguration : IEntityTypeConfiguration<Trip>
+    {
+        public void Configure(EntityTypeBuilder<Trip> builder)
+        {
+            builder.HasData(
+                    new Trip
+                    {
+                        ID = 1,
+                        Details = "To and from Australia"
+                    },
+                    new Trip
+                    {
+                        ID = 2,
+                        Details = "To and from United Kingdom"
+                    }
+                    );
+
+        }
+    }
+}
---
+++ Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Project3.Server.Models;
+using Project3.Shared.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project3.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+
+        public DbSet<Route> Routes { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Traveller> Travellers { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+    }
+}
